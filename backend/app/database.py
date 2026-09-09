863cff88cd717d0f94ce8610b7db7a36d425d808
Gaurangi <gaurangiraul06@gmail.com>
2026-09-08 21:22:07 -0700

    lint issue fixed

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,10 @@
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
 import os
+
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 load_dotenv()
 
 
